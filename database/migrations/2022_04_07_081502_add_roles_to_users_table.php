8836956640960f33c604f0f9851a37b4179af7b1
anjun65 <hajrulkhaira@gmail.com>
2022-04-11 11:25:10 +0700

    test

--- database/migrations/2022_04_07_081502_add_roles_to_users_table.php
+++ database/migrations/2022_04_07_081502_add_roles_to_users_table.php
@@ -14,7 +14,7 @@ class AddRolesToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('roles')->default('USER');
         });
     }
 
