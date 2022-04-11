0b713884772eb24ff132e04ad97831c40c3bf71b
anjun65 <hajrulkhaira@gmail.com>
2022-04-11 13:12:31 +0700

    debugging

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -120,7 +120,7 @@
                             PPFTZ
                         </a>
 
-                        <a href="/admin/surat-kuasa" class="group flex items-center px-2 py-2 text-sm leading-5 font-medium text-gray-900 rounded-md bg-white focus:outline-none focus:bg-gray-700 focus:text-white transition ease-in-out duration-150">
+                        <a href="/admin/dokumen-ppjk" class="group flex items-center px-2 py-2 text-sm leading-5 font-medium text-gray-900 rounded-md bg-white focus:outline-none focus:bg-gray-700 focus:text-white transition ease-in-out duration-150">
                             <svg class="mr-3 h-6 w-6 text-gray-900 group-focus:text-gray-300 transition ease-in-out duration-150" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
                             </svg>
