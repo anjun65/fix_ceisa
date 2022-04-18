f2eb5d98cc69509c286df23ad66500a573fddfac
anjun65 <hajrulkhaira@gmail.com>
2022-04-18 15:06:00 +0700

    test

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
+    <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
+    <title>CEISA Polibatam</title>
 
     <!-- Tailwind UI -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tailwindcss/ui@latest/dist/tailwind-ui.min.css">
